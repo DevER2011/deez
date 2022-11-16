6b406bc52616588203571d1477b8e4119e4674ff
DevER2011 <88580138+DevER2011@users.noreply.github.com>
2022-11-17 07:50:06 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/o2xEJ8dfn/model.json',modelReady);
+    }
+    function modelReady(){
+    classifier.classify(gotResults);
+    }
+    function gotResults(error, results){
+        if(error){
+        console.error(error);
+        } else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) +1;
+        random_number_g = Math.floor(Math.random() * 255) +1;
+        random_number_b = Math.floor(Math.random() * 255) +1;
+        
+        
+        document.getElementById("result_label").innerHTMl = 'I can Hear -'+ results[0].label;
+        document.getElementById("result_confidence").innerHTMl = 'Accuracy -'+ (results[0].confidence*100).toFixed(2)+"%";
+        
+        document.getElementById("result_label").style.color = "rgb("+ random_number_r+","+random_number_g+","+random_number_r+")";
+        document.getElementById("result_confidence").style.color = "rgb("+ random_number_r+","+random_number_g+","+random_number_r+")";
+        
+        
+        img = document.getElementById('dog');
+        img1 = document.getElementById('cat');
+        img2 = document.getElementById('cow');
+        
+        if(results[0].label == "mooing"){
+            img.src = 'go.gif';
+            img1.src = 'download.jpg';
+            img2.src = 'download.png';
+        }else if(results[0].label == "raging"){
+            img.src = '49-496054_cute-puppy-dog-clip-art-transparent-cartoon-png.png';
+            img1.src = 'giphy.gif';
+            img2.src = 'download.png';
+        } else if(results[0].label == "water shaking"){
+            img.src = '49-496054_cute-puppy-dog-clip-art-transparent-cartoon-png.png';
+            img1.src = 'download.jpg';
+            img2.src = 'dd.gif';
+        }
+        }
+        }
